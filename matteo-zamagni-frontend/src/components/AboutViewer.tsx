2f3caa76d91f5d6da0ccb5bfb1ac62488029812f
Hari Chauhan <hari-c@hotmail.co.uk>
2024-07-31 12:27:46 +0100

    Update about viewer mobile layout

--- matteo-zamagni-frontend/src/components/AboutViewer.tsx
+++ matteo-zamagni-frontend/src/components/AboutViewer.tsx
@@ -19,18 +19,18 @@ export const AboutViewer: React.FC<AboutViewerProps> = ({ content }) => {
           case StrapiAboutComponentType.Year:
             return (
               <div className="flex justify-end">
-                <p className="text-lg text-right w-2/3 border-b-[1px] border-black py-2">
+                <p className="text-lg text-right w-full md:w-2/3 border-b-[1px] border-black py-2">
                   {item.Year}
                 </p>
               </div>
             );
           case StrapiAboutComponentType.Item:
             return (
-              <div className="flex mt-2">
-                <div className="w-1/3">
+              <div className="flex-wrap md:flex-nowrap flex mt-2">
+                <div className="w-full md:w-1/3 mb-4 md:mb-0 font-bold md:font-normal">
                   <p>{item.Label}</p>
                 </div>
-                <div className="w-2/3 flex">
+                <div className="w-full md:w-2/3 flex">
                   <div className="w-1/2 border-b-[1px] border-black pb-2">
                     <p>{item.Name}</p>
                   </div>
