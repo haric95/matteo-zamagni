2fce98c7cffae60f9aec72cc39fb2ec8bd29fd45
Hari Chauhan <hari-c@hotmail.co.uk>
2024-07-31 18:34:54 +0100

    Increase populate deep depth

--- matteo-zamagni-cms/config/plugins.js
+++ matteo-zamagni-cms/config/plugins.js
@@ -15,7 +15,7 @@ module.exports = ({ env }) => ({
   },
   "strapi-plugin-populate-deep": {
     config: {
-      defaultDepth: 5, // Default is 5
+      defaultDepth: 6, // Default is 5
     },
   },
 });
